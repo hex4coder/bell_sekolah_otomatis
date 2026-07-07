c6e71d35eb08672b35ce375de4f1915608c0b6ea
hex4coder <the.programmer.luyo@gmail.com>
2026-07-07 19:51:49 +0800

    Add AdminSeeder to database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
+            AdminSeeder::class,
             SchoolDaySeeder::class,
             AudioAssetSeeder::class,
             BellScheduleSeeder::class,
