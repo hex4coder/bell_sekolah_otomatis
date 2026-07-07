e15e256598d597689c17a239dbf70e76c591466a
hex4coder <the.programmer.luyo@gmail.com>
2026-07-07 19:31:29 +0800

    menambahkan updater.sh untuk auto exec command update aplikasi

---
+++ updater.sh
@@ -0,0 +1,12 @@
+git stash
+git clean -df
+git pull
+
+# 1. Reset file database SQLite agar kosong kembali
+> /var/www/bell_sekolah_otomatis/database/database.sqlite
+
+# 2. Jalankan rangkaian perintah andalan Anda
+su -s /bin/bash -c "composer install --no-dev" www-data
+su -s /bin/bash -c "php artisan key:generate" www-data
+su -s /bin/bash -c "php artisan migrate --seed" www-data
+su -s /bin/bash -c "php artisan config:cache && php artisan route:cache && php artisan view:cache" www-data
