aaf6574602f48ec2b98a678e3ff34ca5c0b2de5d
hex4coder <the.programmer.luyo@gmail.com>
2026-07-08 10:31:35 +0800

    nonaktifkan reset database di updater

--- updater.sh
+++ updater.sh
@@ -5,7 +5,7 @@ git stash
 git pull
 
 # 1. Reset file database SQLite agar kosong kembali
-> /var/www/bell_sekolah_otomatis/database/database.sqlite
+#> /var/www/bell_sekolah_otomatis/database/database.sqlite
 
 # 2. Jalankan rangkaian perintah andalan Anda
 su -s /bin/bash -c "composer install --no-dev" www-data
