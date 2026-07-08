229067fb23e9a0f76bd9500935c6205e3eb1aaec
hex4coder <the.programmer.luyo@gmail.com>
2026-07-08 13:07:53 +0800

    hapus Breeze default dashboard, redirect /dashboard ke /admin/dashboard

--- routes/web.php
+++ routes/web.php
@@ -60,9 +60,7 @@ Route::get('/', function () {
     ]);
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::redirect('/dashboard', '/admin/dashboard')->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
