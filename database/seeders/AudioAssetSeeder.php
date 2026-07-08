320d52de633e4990d60e75633cb258d95311237c
hex4coder <the.programmer.luyo@gmail.com>
2026-07-08 09:54:10 +0800

    sync seeder with all audio assets and update bell schedule

--- database/seeders/AudioAssetSeeder.php
+++ database/seeders/AudioAssetSeeder.php
@@ -10,7 +10,21 @@ class AudioAssetSeeder extends Seeder
     public function run(): void
     {
         $files = [
+            '5_menit_akhir_istirahat.wav' => '5 Menit Akhir Istirahat',
+            '5_menit_akhir_istirahat_1.wav' => '5 Menit Akhir Istirahat 1',
+            '5_menit_akhir_istirahat_2.wav' => '5 Menit Akhir Istirahat 2',
             '5_menit_awal_jam_ke_1.wav' => '5 Menit Awal Jam ke-1',
+            '5_menit_awal_kegiatan_keagamaan.wav' => '5 Menit Awal Kegiatan Keagamaan',
+            '5_menit_awal_upacara.wav' => '5 Menit Awal Upacara',
+            'akhir_pekan_1.wav' => 'Akhir Pekan 1',
+            'akhir_pekan_2.wav' => 'Akhir Pekan 2',
+            'akhir_pelajaran_1.wav' => 'Pulang',
+            'akhir_pelajaran_2.wav' => 'Akhir Pelajaran 2',
+            'ayo_senam.wav' => 'Ayo Senam',
+            'ayo_senam.mp3' => 'Ayo Senam MP3',
+            'istirahat.wav' => 'Istirahat',
+            'istirahat_1.wav' => 'Istirahat 1',
+            'istirahat_2.wav' => 'Istirahat 2',
             'jam_ke_1.wav' => 'Jam ke-1',
             'jam_ke_2.wav' => 'Jam ke-2',
             'jam_ke_3.wav' => 'Jam ke-3',
@@ -21,8 +35,14 @@ class AudioAssetSeeder extends Seeder
             'jam_ke_8.wav' => 'Jam ke-8',
             'jam_ke_9.wav' => 'Jam ke-9',
             'jam_ke_10.wav' => 'Jam ke-10',
-            'istirahat.wav' => 'Istirahat',
-            'akhir_pelajaran_1.wav' => 'Pulang',
+            'jam_ke_11.wav' => 'Jam ke-11',
+            'jam_ke_12.wav' => 'Jam ke-12',
+            'jam_ke_13.wav' => 'Jam ke-13',
+            'jam_ke_14.wav' => 'Jam ke-14',
+            'mars_smk.wav' => 'Mars SMK',
+            'pelajar_pancasila.wav' => 'Pelajar Pancasila',
+            'sholawat_jibril.wav' => 'Sholawat Jibril',
+            '1783411183_audiobelljam10.wav' => 'Lagu Indonesia Raya',
         ];
 
         foreach ($files as $filename => $name) {
--- database/seeders/BellScheduleSeeder.php
+++ database/seeders/BellScheduleSeeder.php
@@ -12,6 +12,7 @@ class BellScheduleSeeder extends Seeder
         $audioMap = [
             '5 Menit Awal Jam ke-1' => '5_menit_awal_jam_ke_1.wav',
             '5 Menit Awal Upacara' => '5_menit_awal_upacara.wav',
+            '5 Menit Awal Kegiatan Keagamaan' => '5_menit_awal_kegiatan_keagamaan.wav',
             'Jam ke-1' => 'jam_ke_1.wav',
             'Jam ke-2' => 'jam_ke_2.wav',
             'Jam ke-3' => 'jam_ke_3.wav',
@@ -22,13 +23,25 @@ class BellScheduleSeeder extends Seeder
             'Jam ke-8' => 'jam_ke_8.wav',
             'Jam ke-9' => 'jam_ke_9.wav',
             'Jam ke-10' => 'jam_ke_10.wav',
+            'Jam ke-11' => 'jam_ke_11.wav',
+            'Jam ke-12' => 'jam_ke_12.wav',
+            'Jam ke-13' => 'jam_ke_13.wav',
+            'Jam ke-14' => 'jam_ke_14.wav',
             'Istirahat 1' => 'istirahat_1.wav',
+            'Istirahat 2' => 'istirahat_2.wav',
+            'Istirahat' => 'istirahat.wav',
             'Akhir Istirahat 1' => '5_menit_akhir_istirahat_1.wav',
             'Akhir Istirahat 2' => '5_menit_akhir_istirahat_2.wav',
             'Akhir Istirahat' => '5_menit_akhir_istirahat.wav',
-            'Istirahat 2' => 'istirahat_2.wav',
-            'Istirahat' => 'istirahat.wav',
             'Pulang' => 'akhir_pelajaran_1.wav',
+            'Akhir Pelajaran 2' => 'akhir_pelajaran_2.wav',
+            'Akhir Pekan 1' => 'akhir_pekan_1.wav',
+            'Akhir Pekan 2' => 'akhir_pekan_2.wav',
+            'Ayo Senam' => 'ayo_senam.wav',
+            'Mars SMK' => 'mars_smk.wav',
+            'Pelajar Pancasila' => 'pelajar_pancasila.wav',
+            'Sholawat Jibril' => 'sholawat_jibril.wav',
+            'Lagu Indonesia Raya' => '1783411183_audiobelljam10.wav',
         ];
 
         $monday = [
@@ -46,7 +59,9 @@ class BellScheduleSeeder extends Seeder
             ['time' => '12:15', 'name' => 'Jam ke-6'],
             ['time' => '12:55', 'name' => 'Jam ke-7'],
             ['time' => '13:35', 'name' => 'Jam ke-8'],
-            ['time' => '15:00', 'name' => 'Pulang'],
+            ['time' => '14:15', 'name' => 'Jam ke-9'],
+            ['time' => '14:55', 'name' => 'Jam ke-10'],
+            ['time' => '15:35', 'name' => 'Pulang'],
         ];
 
         $tueToThu = [
@@ -65,7 +80,8 @@ class BellScheduleSeeder extends Seeder
             ['time' => '12:55', 'name' => 'Jam ke-8'],
             ['time' => '13:35', 'name' => 'Jam ke-9'],
             ['time' => '14:15', 'name' => 'Jam ke-10'],
-            ['time' => '15:40', 'name' => 'Pulang'],
+            ['time' => '14:55', 'name' => 'Jam ke-11'],
+            ['time' => '15:35', 'name' => 'Pulang'],
         ];
 
         $friday = [
